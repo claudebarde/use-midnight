3574fb6745bd675556557b84b95125fd29b7f701
Claude Barde <25201109+claudebarde@users.noreply.github.com>
2024-12-09 17:27:09 +0000

    fixed bug with module version

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,2 +1,11 @@
+// import type { DAppConnectorAPI } from "./api";
 declare module "*.css";
 declare module "*.svg";
+
+declare global {
+  interface Window {
+    midnight?: {
+      [key: string]: DAppConnectorAPI;
+    };
+  }
+}
